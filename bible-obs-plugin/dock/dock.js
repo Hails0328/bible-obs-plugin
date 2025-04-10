9bdd8d82869a5ef1f11a868e1a506552e99e3480
Hails0328 <camo.bugg@gmail.com>
2025-04-10 01:12:28 -0400

    Add files via upload

---
+++ bible-obs-plugin/dock/dock.js
@@ -0,0 +1,75 @@
+
+let bible = {};
+let selectedBook = '';
+let selectedChapter = 1;
+
+async function loadBible() {
+  const res = await fetch('https://raw.githubusercontent.com/Hails0328/BibleOBS/main/EntireBible-NIV.json');
+  bible = await res.json();
+
+  const bookSelect = document.getElementById('book');
+  Object.keys(bible).forEach(book => {
+    const option = document.createElement('option');
+    option.value = book;
+    option.textContent = book;
+    bookSelect.appendChild(option);
+  });
+
+  bookSelect.addEventListener('change', e => {
+    selectedBook = e.target.value;
+    populateChapters();
+  });
+
+  document.getElementById('chapter').addEventListener('change', e => {
+    selectedChapter = e.target.value;
+    populateVerses();
+  });
+
+  bookSelect.value = Object.keys(bible)[0];
+  bookSelect.dispatchEvent(new Event('change'));
+}
+
+function populateChapters() {
+  const chapterSelect = document.getElementById('chapter');
+  chapterSelect.innerHTML = '';
+  const chapters = Object.keys(bible[selectedBook]);
+  chapters.forEach(ch => {
+    const option = document.createElement('option');
+    option.value = ch;
+    option.textContent = ch;
+    chapterSelect.appendChild(option);
+  });
+  chapterSelect.value = chapters[0];
+  chapterSelect.dispatchEvent(new Event('change'));
+}
+
+function populateVerses() {
+  const start = document.getElementById('startVerse');
+  const end = document.getElementById('endVerse');
+  start.innerHTML = '';
+  end.innerHTML = '';
+  const verses = Object.keys(bible[selectedBook][selectedChapter]);
+  verses.forEach(v => {
+    const o1 = document.createElement('option');
+    const o2 = document.createElement('option');
+    o1.value = v;
+    o1.textContent = v;
+    o2.value = v;
+    o2.textContent = v;
+    start.appendChild(o1);
+    end.appendChild(o2);
+  });
+}
+
+function updateDisplay() {
+  const s = document.getElementById('startVerse').value;
+  const e = document.getElementById('endVerse').value;
+  const url = new URL('../display/index.html', window.location.href);
+  url.searchParams.set('book', selectedBook);
+  url.searchParams.set('chapter', selectedChapter);
+  url.searchParams.set('start', s);
+  url.searchParams.set('end', e);
+  localStorage.setItem('verseRef', url.search);
+}
+
+loadBible();
